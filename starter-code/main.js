fe762fea0aeeed5fe06ecb20be527705082efd55
Ryne Schillinger <ryneschillinger@gmail.com>
2016-11-25 20:07:55 -0800

    Added game logic to JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+	alert ("Sorry, try again.");
+}
