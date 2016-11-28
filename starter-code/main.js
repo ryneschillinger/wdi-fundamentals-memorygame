144fcf1fc3cfe684696dc78e9c6d4fbb2a878b38
Ryne Schillinger <ryneschillinger@gmail.com>
2016-11-28 07:30:42 -0800

    final solution

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,41 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else if (cardThree === cardFour) {
-	alert("You found a match!");
-} else {
-	alert ("Sorry, try again.");
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
 }
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<h1>K</h1>"; // king
+	} else {
+		this.innerHTML = "<h1>Q</h1>"; //queen
+	}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
